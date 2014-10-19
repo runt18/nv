75bb72bbe1d8e963d189f9b3e663c2be3e94b73b
Zachary Waldowski <zach@waldowski.me>
2014-10-19 10:34:07 -0400

    Add macros header

---
+++ NTVMacros.h
@@ -0,0 +1,12 @@
+//
+//  NTVMacros.h
+//  Notation
+//
+//  Created by Zachary Waldowski on 8/23/14.
+//  Copyright (c) 2014 ElasticThreads. All rights reserved.
+//
+
+#ifndef NTV_MACROS
+#define NTV_MACROS
+
+#endif /* !NTV_MACROS */
